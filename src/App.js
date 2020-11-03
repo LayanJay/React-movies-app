d1059162c52032ea4910aa4bb4783dfbc9f78597
Layan <layanjayasinghe2@gmail.com>
2020-11-03 20:34:55 +0530

    mobile responsive search function added

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ function App() {
     fetch(API)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
         setMovies(data.results);
       });
   };
@@ -40,10 +39,16 @@ function App() {
     setSearchValue(e.target.value);
   };
 
+  const home = () => {
+    getMovies(FEACHERED_API);
+  };
+
   return (
     <>
       <div className="header">
-        <div className="logo">MovieHub</div>
+        <div className="logo" onClick={home}>
+          MovieHub
+        </div>
         <form onSubmit={handleOnSubmit}>
           <input
             type="search"
@@ -53,6 +58,12 @@ function App() {
             value={searchValue}
             onChange={handleOnChange}
           />
+          <button type="submit" className="submit-btn">
+            <img
+              src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAABmJLR0QA/wD/AP+gvaeTAAACmElEQVRIie2Wz04TURTGv3OnrSFQG4kQ/BNj0IUJuFH3oogrEwsxPoAsCISoG+MbGIRXkEdQhBUJwTUaWSgsTJQYjRHS3jL3ThetOtPPxbT4r7QzVVzxrSb3nu/8Tu7MOXOBff0nSbMAkqlta7OAZAmcA3C8uvUJwKqA852ZzFMR+frPwNp1R0TUDIHeJnk2QLl3+NDBuahgVW+RpCoYMw1Rjwn0EvIagjviqD6/XOrwy6UOCVS/EHcBrgE4BeETbbwpknVzRlLBmGltLLWx5bwxY42SkVTamHFtbDn0eFMtQbXrjuxA3eLFqL686w7swF0vGwtKMlUwdkMby7wxY7GLtnaiWvRbksnIxoIxN0Oo96qVd0XS0casaWNZMOZGo9jfksswAIjwkYhU4oJFJBDKbFiENDzuX8DVPoUotRwXuqOKWgqrwIXIYABHASAolT62zK2Ua95jccB/LaVU02lYD/wZAJy2thOtgkVSNe9WHPBLAIBfGWoVTOHV8AErkcECzlfNo622ExVvAYAIF+IYU9rYd+EQMONxwdraybCH7QbJRDyz6w3/GJnuQFRf3nUvaWO/aGOZ97xrzeL/OM7w1yYPARwQUYva2gmSzm4JSDra2kkRtQggBSAXiLxoBq776ZNUBVt8APB+dWVdKLOoqKUgKH0AgESi7SSlMkSFURB9VWsOQDfAdd9xBnvS6VwscE3a9bIQzgA43SiOwHsoue2LPE8GwTNA+pvBo1x9ktvWXiclC8F5hFcfAbAJYkWEC52ZzJyI+ACwVSx2J4JguRk80pSJq81isSsZws8CeMNk4nJXe/vmnoOjwP/5rK7pSDqd9x3nCsB1AGfkmz//8/6egQGgJ53O+Y4zCGAVQLCXrH3tqu/qL3t6BBo3+gAAAABJRU5ErkJggg=="
+              alt="search"
+            />
+          </button>
         </form>
       </div>
       <div className="App">
